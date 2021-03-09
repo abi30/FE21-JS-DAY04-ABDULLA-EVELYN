a6c76e289762b4f73f7b2616631678f04940fea7
Abdulla Al Rakib <36118180+abi30@users.noreply.github.com>
2021-03-09 14:31:57 +0100

    done_intermediate

--- intermediate/script_1.js
+++ intermediate/script_1.js
@@ -1,21 +1,17 @@
-function santaDisappear () {
-    let menuStatus = false;
 
-// document.getElementById("menu").style.marginLeft = "-300px";
-    if(menuStatus == false) {
-        document.getElementsbyClassName("SantaClaus").style.visibility="visible";
-        document.getElementById(Santa1).style.visibility="visible";
-        // document.getElementById("menu").style.marginLeft = "0px";
-        // document.getElementById("menu").style.transition = "1s";
-        menuStatus = true;
-    }else {
-        document.getElementsByClassName("SantaClaus").style.visibility="hidden";
-        document.getElementById(Santa1).style.visibility="hidden";
-        // document.getElementById("menu").style.marginLeft = "-300px";
-        menuStatus = false;
-    }
-document.getElementsByClassName("container").addEventListener("click",santaDisappear);
+for(let i =0;i<4;i++){
+
+ document.querySelectorAll('.SantaClaus')[i].addEventListener("click",()=>{
+     document.getElementsByClassName("SantaClaus")[i].style.visibility= "hidden"
+    //  document.getElementsByClassName("SantaClaus")[1].style.visibility= "hidden"
+    //  document.getElementsByClassName("SantaClaus")[2].style.visibility= "hidden"
+    //  document.getElementsByClassName("SantaClaus")[3].style.visibility= "hidden"
+
+ })
+
+
+
+
 }
 
-
-
+    
