b1565a48584b2f29c9fa283d1a5cfd77351ed462
Evelyn Gistelinck <evelyngistelinck@hotmail.com>
2021-03-09 15:03:52 +0100

    advanced ex start

--- intermediate/script_1.js
+++ intermediate/script_1.js
@@ -2,7 +2,8 @@
 for(let i =0;i<4;i++){
 
  document.querySelectorAll('.SantaClaus')[i].addEventListener("click",()=>{
-     document.getElementsByClassName("SantaClaus")[i].style.visibility= "hidden"
+    document.getElementsByClassName("SantaClaus")[i].style.visibility= "hidden"
+    //  document.getElementsByClassName("SantaClaus")[0].style.visibility= "hidden"
     //  document.getElementsByClassName("SantaClaus")[1].style.visibility= "hidden"
     //  document.getElementsByClassName("SantaClaus")[2].style.visibility= "hidden"
     //  document.getElementsByClassName("SantaClaus")[3].style.visibility= "hidden"
